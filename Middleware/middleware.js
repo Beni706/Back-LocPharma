d45c9e45fd7de680ecb5ba2acd60742d88daf326
Beni Valerien <benivalerien@gmail.com>
2025-07-21 14:05:12 +0100

    Middleware

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -5,7 +5,7 @@ const app = express()
 // Middlewaires
 export const configurationMiddleware = (app) => {
     app.use(cors({
-        origin: ['https://pharmalabs.onrender.com'], // ton frontend
+        origin: ['https://pharmalabs.onrender.com/'], // ton frontend
         credentials: true,
     }))
     app.use(express.json());
